55360e13847d4f5d1505973b337b2d865367c2e9
Dhruv Shah <dhruvshah@Dhruvs-MacBook-Pro.local>
2021-01-13 23:12:16 -0600

    new config

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,9 @@ export default {
 
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
-
+  router: {
+    base: '/ToDo-App/'
+  },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'day1',
